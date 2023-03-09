ce093c85b89888c51f4d9ebbc92d7e33f72d236d
JuanPabloCostas <jcostas29@alumnos.uaq.mx>
2023-03-09 09:41:48 -0600

    cum in disguise

--- EstructuraDeDatos/entrada-salida.cpp
+++ EstructuraDeDatos/entrada-salida.cpp
@@ -9,24 +9,35 @@ using namespace std;
 
 
 int main(){
-    int outval = 19;
-    string namefile;
+    ofstream WriteFile; 
+    string namefile, texto;
     cout << "Escribe el nombre del archivo: ";
-    cin >> namefile;
+    //cin >> namefile;
+    getline(cin,namefile);
     cout << "\n";
+    WriteFile.open(namefile);
+    cout << "Escribe el texto del archivo: ";
+    getline(cin,texto);
+    cout << "\n";
+    //cout << texto;
 
-    ofstream WriteFile(namefile); //apertura archivo en modo escritura
-    WriteFile << outval << "\n";
+    //apertura archivo en modo escritura
+    //WriteFile.open(namefile.c_str());
+    WriteFile << texto << "\n";
 
     WriteFile.close();
 
-    int inval;
-    ifstream ReadFile(namefile);
+    string inval;
+    ifstream ReadFile(namefile.c_str());
+    if (ReadFile.fail()){
+        cout << "El archivo no se encuentra";
+        exit(1);
+    }
+    else{
     ReadFile >> inval;
-
     ReadFile.close();
 
-    cout<<"El valor escrito en el archivo es: " << inval << "\n";
+    cout<<"El valor escrito en el archivo es: " + inval + "\n";
     return 1;
-
+    }
 }
